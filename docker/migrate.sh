367dd2628ce4cda4e8d96ffee83e6339a743cf4a
sho <ongsihui01@gmail.com>
2023-07-17 17:20:58 +0800

    Modify docker-compose to fix working-dir

--- docker/migrate.sh
+++ docker/migrate.sh
@@ -1,7 +1,7 @@
 BASEDIR=$(dirname $0)
 
 # Migrate SQL tables
-docker exec --workdir=/app/myapp csc1106_webapp php spark migrate
+docker exec csc1106_webapp php spark migrate
 
 # Create temporary default user
 docker exec -i csc1106_mariadb mysql -u mariadb_user -pmariadb_password webapp_db < $BASEDIR/create_user.sql
