50281fb390a3d4a02894883fbec09a8aad4935c9
Aaron <mcdulltii@gmail.com>
2023-07-23 17:02:07 +0800

    Modified sql tables

--- docker/mysql-init.sql
+++ docker/mysql-init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `user` (
-  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(255) NOT NULL,
   `user_name` VARCHAR(255) NOT NULL,
   `user_password_hash` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `form` (
-  `form_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `form_id` VARCHAR(255) NOT NULL,
   `form_blob` TEXT NOT NULL,
-  `user_id` INT UNSIGNED NOT NULL,
+  `user_id` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`form_id`),
   FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
